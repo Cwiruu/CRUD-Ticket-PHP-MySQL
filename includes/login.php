efa208b3f355e71517ae08676d2583e0a1857445
Łukasz Ćwir <cwirlukasz@gmail.com>
2020-01-26 21:46:02 +0100

    Update login.php

--- includes/login.php
+++ includes/login.php
@@ -1,43 +1,40 @@
-<?php include "db.php"; ?>
-<?php session_start(); ?>
-
-<?php
+<?php 
+include "db.php";
+session_start();
 
 if (isset($_POST['login'])) {
     $userid = $_POST['userid'];
     $password = $_POST['password'];
 
+    $userid = stripslashes($userid);
+    $password = stripslashes($password);
     $userid = mysqli_real_escape_string($connection, $userid);
     $password = mysqli_real_escape_string($connection, $password);
 
-    $query = "SELECT * FROM user WHERE id_user = $userid";
+    $query = "SELECT * FROM user WHERE id_user = '$userid' and user_password = '$password' ";
     $select_user_query = mysqli_query($connection, $query);
 
-    if (!$select_user_query) {
-        die("Query failed" . mysqli_error($connection));
-    }
+    // if (!$select_user_query) {
+    //     die("Query failed" . mysqli_error($connection));
+    // }
 
     while ($row = mysqli_fetch_array($select_user_query)) {
         $ID_USER = $row['ID_USER'];
-        $FIRST_NAME = $row['FIRST_NAME'];
-        $EMAIL = $row['EMAIL'];
-        $USER_TYPE = $row['USER_TYPE'];
         $USER_PASSWORD = $row['USER_PASSWORD'];
     }
 }
 
-if ($userid !== $ID_USER && $password != $USER_PASSWORD) {
-    header("Location: ../index.php");
-} else if ($userid == $ID_USER && $password == $USER_PASSWORD) {
+if (!empty($userid) && !empty($password) && $userid === $ID_USER && $password === $USER_PASSWORD && $userid != "" && $password != "") {
 
     $_SESSION['userid'] = $ID_USER;
-    $_SESSION['firstname'] = $FIRST_NAME;
-    $_SESSION['email'] = $EMAIL;
-    $_SESSION['USER_TYPE'] = $USER_TYPE;
+    // $_SESSION['firstname'] = $FIRST_NAME;
+    // $_SESSION['email'] = $EMAIL;
+    // $_SESSION['USER_TYPE'] = $USER_TYPE;
 
-    header("Location: ../admin");
+    header("Location: ../main.php");
 } else {
     header("Location: ../index.php");
 }
+
 
 ?>
